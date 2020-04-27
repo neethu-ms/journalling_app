e1b9c92e74067700ef9ef7a2f724de2416343dc5
neethu-ms <msneethu2989@gmail.com>
2020-04-27 19:38:36 +0000

    Refactor code

--- client/src/components/Bio/Profile.js
+++ client/src/components/Bio/Profile.js
@@ -1,27 +1,14 @@
- /* 
-  props:
-   username:String
-   Level:Integer
-
-*/
-
 import React from "react";
 import "./Profile.scss"
-import {Container} from '@material-ui/core';
+import { Container } from '@material-ui/core';
 
-export default function UserBio(props){
-
-
+export default function UserBio(props) {
   return (
     <aside className="" id="">
       <Container>
-       {/* <img id="" src={"https://image.freepik.com/free-photo/woman-smiling-face-with-curly-hair_1139-582.jpg"} alt="profile pic"/> */}
-  {/* <img src="images/journaling1.jpg" alt="home-logo"></img> */}
-        
-    {/*   <hr className="seperator"/> */}
-      <section className="prompt__actions card_right">
+        <section className="prompt__actions card_right">
           <h4 className="">Level: {props.level}</h4>
-      </section>
+        </section>
       </Container>
     </aside>
   );
