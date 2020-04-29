24cbba228d622e3dbb1e5c7d3ddc2ed326e8f79f
neethu-ms <msneethu2989@gmail.com>
2020-04-29 01:19:26 +0000

    Add style

--- client/src/components/Bio/Profile.js
+++ client/src/components/Bio/Profile.js
@@ -1,23 +1,20 @@
 import React from "react";
 import "./Profile.scss"
-import { Container, Avatar, FormHelperText} from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
-  levelStyle:{
-    backgroundColor:"#03A9F4",
+  levelStyle: {
+    backgroundColor: "#03A9F4",
     height: "50px",
-    width:"100px",
-    color:"black",
-    alignSelf:"center"
+    width: "100px",
+    color: "black",
+    alignSelf: "center"
   },
-
-  containerStyle:{
-    display:"flex",
-      alignItems:"flex-end"
+  containerStyle: {
+    display: "flex",
+    alignItems: "flex-end"
   }
-      
-    }
-    
+}
 );
 
 export default function UserBio(props) {
@@ -25,10 +22,7 @@ export default function UserBio(props) {
   return (
     <aside className="" id="">
       <Container className={classes.containerStyle}>
-       
-        <Avatar variant="square" className={classes.levelStyle}>Level:{props.level}</Avatar>
-          {/*<h4 className="">Level: {props.level}</h4>*/}
-               
+        <h4 className="">Level: {props.level}</h4>
       </Container>
     </aside>
   );
