619829fcd3d07f735933a2dee3b9759ba827c84e
neethu-ms <msneethu2989@gmail.com>
2020-05-02 00:46:23 +0000

    Add bcrypt

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -5,29 +5,27 @@ let db = require("../db/models/index");
 // bcrypt
 const bcrypt = require("bcrypt");
 
-//Get users
+//login
 router.post("/", (req, res) => {
-  //console.log("req.body",req.body);
   db.user
-  .findOne({
-    where: { email: req.body.email},
-  })
+    .findOne({
+      where: { email: req.body.email },
+    })
     .then((user) => {
-      console.log('data',user.dataValues);
-     bcrypt.compare(req.body.password,user.dataValues.password).then(() => {
-      req.session.email = req.body.email; 
-      return res.json(user.dataValues);
-    
+      console.log("data", user.dataValues);
+      bcrypt
+        .compare(req.body.password, user.dataValues.password)
+        .then(() => {
+          req.session.email = req.body.email;
+          return res.json(user.dataValues);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
     });
-})
-.catch((err) => {
-  res.status(500).json({ error: err.message });
 });
-});
-
-
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,58 +1,70 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let db = require('../db/models/index');
+let db = require("../db/models/index");
+
+// bcrypt
+const bcrypt = require("bcrypt");
 
 //Get users
 router.get("/", (req, res) => {
-  db.user.findAll().then(users => {
-    res.json(users);
-  })
-    .catch(err => {
+  db.user
+    .findAll()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
       res.status(500).json({ error: err.message });
     });
 });
 
 //Get specific user
 router.get("/:id", (req, res) => {
-  db.user.findOne({
-    where: { id: req.params.id }
-  }).then(users => {
-    res.json(users);
-  })
-    .catch(err => {
+  db.user
+    .findOne({
+      where: { id: req.params.id },
+    })
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
       res.status(500).json({ error: err.message });
     });
 });
 
-
 //Create  user
 router.post("/", (req, res) => {
-  db.user.create({
-    handle: req.body.handle, email: req.body.email, password: req.body.password, points: req.body.points, 
-    //journalNo: req.body.journalNo,
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString()
-  })
-    .then(users => {
-      res.json(users);
-
+  bcrypt.hash(req.body.password, 10).then(password => {
+  db.user
+    .create({
+      handle: req.body.handle,
+      email: req.body.email,
+      password: password,
+      points: req.body.points,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
     })
-    .catch(err => {
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
       res.status(500).json({ error: err.message });
     });
+  });
 });
 
 //Update  user score
 router.put("/", (req, res) => {
-   db.user.update({
-    points:req.body.points},
-    {where: {id:req.body.id}}
+  db.user
+    .update(
+      {
+        points: req.body.points,
+      },
+      { where: { id: req.body.id } }
     )
-    .then(users => {
+    .then((users) => {
       res.json(users);
-
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({ error: err.message });
     });
 });
