2eb3056c9e9668dba03272b9d8cf616c54603567
neethu-ms <msneethu2989@gmail.com>
2020-05-04 00:03:35 +0000

    Refactor code

--- client/cypress/integration/navigation.spec.js
+++ client/cypress/integration/navigation.spec.js
@@ -1,69 +1,56 @@
-describe("Navigation",()=> {
-  it("should visit root",()=>{
+describe("Navigation", () => {
+  it("should visit root", () => {
     cy.visit("/");
   });
 
-    // It should REGISTER a user
-  it('registers user',() =>{
-       cy.request("GET","http://localhost:3001/api/db/reset");
-       cy.visit('/');
-       cy.contains("REGISTER").click();
-       cy.contains("Email Address").type("test4@gmail.com");
-       cy.get("#password").type("test4");
-       cy.get("#biodata").type("I am a carpenter. I would like to make myself organized and motivated.");
-       cy.contains("Submit").click();
-       cy.contains("LOGOUT").click();
-       cy.contains("OK").click();
-    }); 
- // It should LOGIN a user and logout
- it('Login user',() =>{
-  cy.visit('/');
-  cy.contains("LOGIN").click();
-  cy.contains("Email Address").type("test4@gmail.com");
-  cy.get("#password").type("test4");
-   cy.contains("Submit").click();
-   cy.contains("LOGOUT").click();
-   cy.contains("OK").click();
- }); 
+  // It should REGISTER a user
+  it("registers user", () => {
+    cy.request("GET", "http://localhost:3001/api/db/reset");
+    cy.visit("/");
+    cy.contains("REGISTER").click();
+    cy.contains("Email Address").type("test4@gmail.com");
+    cy.get("#password").type("test4");
+    cy.get("#biodata").type(
+      "I am a carpenter. I would like to make myself organized and motivated."
+    );
+    cy.contains("Submit").click();
+    cy.contains("LOGOUT").click();
+    cy.contains("OK").click();
+  });
+  // It should LOGIN a user and logout
+  it("Login user", () => {
+    cy.visit("/");
+    cy.contains("LOGIN").click();
+    cy.contains("Email Address").type("test4@gmail.com");
+    cy.get("#password").type("test4");
+    cy.contains("Submit").click();
+    cy.contains("LOGOUT").click();
+    cy.contains("OK").click();
+  });
 
-it('Level 1 user insights should show a message',() =>{
-  cy.visit('/');
-  cy.contains("LOGIN").click();
-  cy.contains("Email Address").type("test4@gmail.com");
-  cy.get("#password").type("test4");
-   cy.contains("Submit").click();
-  cy.contains("GET INSIGHTS").click(); 
-  cy.contains("Your Personality Analysis").should("not.exist");
-  cy.contains("Reach level 10 to access your insights!").should("exist");
-  cy.contains("BACK").click(); 
-  cy.contains("LOGOUT").click();
-  cy.contains("OK").click();
-}); 
+  it("Level 1 user insights should show a message", () => {
+    cy.visit("/");
+    cy.contains("LOGIN").click();
+    cy.contains("Email Address").type("test4@gmail.com");
+    cy.get("#password").type("test4");
+    cy.contains("Submit").click();
+    cy.contains("GET INSIGHTS").click();
+    cy.contains("Your Personality Analysis").should("not.exist");
+    cy.contains("Reach level 10 to access your insights!").should("exist");
+    cy.contains("BACK").click();
+    cy.contains("LOGOUT").click();
+    cy.contains("OK").click();
+  });
 
-it('User posts multiple messages and it appears in wall',() =>{
-  cy.visit('/');
-  cy.contains("LOGIN").click();
-  cy.contains("Email Address").type("test4@gmail.com");
-  cy.get("#password").type("test4");
-   cy.contains("Submit").click();
-   cy.wait(3000);
+  it("User posts multiple messages and it appears in wall", () => {
+    cy.visit("/");
+    cy.contains("LOGIN").click();
+    cy.contains("Email Address").type("test4@gmail.com");
+    cy.get("#password").type("test4");
+    cy.contains("Submit").click();
+    cy.wait(1000);
     cy.contains("goal").click();
     cy.get("#filled-textarea").type("do exercise");
     cy.contains("Answer").click();
-    cy.contains("goal").click();
-    cy.get("#filled-textarea").type("Do exercise. Make a complete plan for today's activities. Meet Reena and John. Meet some other friends. Buy grocery. Cook chicken and vegetable salads. Go outside and play badminton. Complete the study plan for this week. Start the new project. See some motivating videos. Read some stories of how people overcome difficult situations.");
-    cy.contains("Answer").click();
-    cy.contains("goal").click();
-    cy.get("#filled-textarea").type("Do exercise. Make a complete plan for today's activities. Meet Reena and John. Meet some other friends. Buy grocery. Cook chicken and vegetable salads. Go outside and play badminton. Complete the study plan for this week. Start the new project. See some motivating videos. Read some stories of how people overcome difficult situations.");
-    cy.contains("Answer").click();
-    cy.contains("goal").click();
-    cy.get("#filled-textarea").type("Do exercise. Make a complete plan for today's activities. Meet Reena and John. Meet some other friends. Buy grocery. Cook chicken and vegetable salads. Go outside and play badminton. Complete the study plan for this week. Start the new project. See some motivating videos. Read some stories of how people overcome difficult situations.");
-    cy.contains("Answer").click();
-    cy.contains("goal").click();
-    cy.get("#filled-textarea").type("Do exercise. Make a complete plan for today's activities. Meet Reena and John. Meet some other friends. Buy grocery. Cook chicken and vegetable salads. Go outside and play badminton. Complete the study plan for this week. Start the new project. See some motivating videos. Read some stories of how people overcome difficult situations.");
-    cy.contains("Answer").click();
-}); 
-
+  });
 });
-
-
