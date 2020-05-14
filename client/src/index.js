16db5452f313ca8b6eaf7d5fe2d87e2803c7be8b
neethu-ms <msneethu2989@gmail.com>
2020-05-14 17:05:03 +0000

    Backend api server

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './components/Application';
-import './index.scss';
-
-ReactDOM.render(
-  <Application />,
-  document.getElementById('root')
-);
